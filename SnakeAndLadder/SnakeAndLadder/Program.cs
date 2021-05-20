deb810188c1a8f79bf2d23ca68bdc435466efecb
Rohit1823 <rohitmachale1823@gmail.com>
2021-05-20 13:01:27 +0530

    [Rohit]ADD: Single Player at start position

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Player1 = 0;
+            Console.WriteLine("Player Position is " + Player1);
+        }
+    }
+}
