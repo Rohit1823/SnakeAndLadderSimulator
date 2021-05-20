c9de2235a69ae0958095989b4361ea19a6023379
Rohit1823 <rohitmachale1823@gmail.com>
2021-05-20 13:10:43 +0530

    [Rohit]ADD: Player rolls the die

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -4,10 +4,19 @@ namespace SnakeAndLadder
 {
     class Program
     {
+        public int RollDie()
+        {
+            Random random = new Random();
+            int Player = random.Next(1, 7);
+            return Player;
+        }
         static void Main(string[] args)
         {
             int Player1 = 0;
             Console.WriteLine("Player Position is " + Player1);
+            Program obj = new Program();
+            int DieValue = obj.RollDie();
+            Console.WriteLine("Roll Die value is : " + DieValue);
         }
     }
 }
