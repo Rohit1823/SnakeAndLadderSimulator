66e7c5b19a7b87afe8b1bbabc073e1708f26d846
Rohit1823 <rohitmachale1823@gmail.com>
2021-05-20 13:29:35 +0530

    [Rohit]ADD: Checks for option i.e. No play, Ladder, Snake

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -10,6 +10,31 @@ namespace SnakeAndLadder
             int Player = random.Next(1, 7);
             return Player;
         }
+        public int Check_Option(int DieValue, int Player1)
+        {
+            Random random = new Random();
+            int Option = random.Next(0, 3);
+
+            switch (Option)
+            {
+                case 1:
+                    Console.WriteLine("Ladder");
+                    Player1 = Player1 + DieValue;
+                    break;
+                case 2:
+                    Console.WriteLine("Sneak");
+                    Player1 = Player1 - DieValue;
+                    if (Player1 < 0)
+                    {
+                        Player1 = 0;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("No Play");
+                    break;
+            }
+            return Player1;
+        }
         static void Main(string[] args)
         {
             int Player1 = 0;
@@ -17,6 +42,8 @@ namespace SnakeAndLadder
             Program obj = new Program();
             int DieValue = obj.RollDie();
             Console.WriteLine("Roll Die value is : " + DieValue);
+            Console.WriteLine("Player Status And Value is : " + obj.Check_Option(DieValue, Player1));
         }
-    }
-}
+
+    }   
+}   
