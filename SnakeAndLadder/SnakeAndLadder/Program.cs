e3fdf576879467e32d86cac875324987d4e4adc4
Rohit1823 <rohitmachale1823@gmail.com>
2021-05-20 14:00:56 +0530

    [Rohit]ADD: Player gets to exact winning position

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -10,7 +10,7 @@ namespace SnakeAndLadder
             int Player = random.Next(1, 7);
             return Player;
         }
-        public int Check_Option(int DieValue, int Player1)
+        public int Check_Option(int DieValue, int Player1, int PrePosition)
         {
             Random random = new Random();
             int Option = random.Next(0, 3);
@@ -22,7 +22,7 @@ namespace SnakeAndLadder
                     Player1 = Player1 + DieValue;
                     break;
                 case 2:
-                    Console.WriteLine("Sneak");
+                    Console.WriteLine("Snake");
                     Player1 = Player1 - DieValue;
                     if (Player1 < 0)
                     {
@@ -39,15 +39,20 @@ namespace SnakeAndLadder
         {
             int Player1 = 0;
           
-            while (Player1 <= 100)
+            while (Player1 < 100)
             {
-                Console.WriteLine("Player Position is " + Player1);
+                Console.WriteLine("--------------");
                 Program obj = new Program();
                 int DieValue = obj.RollDie();
                 Console.WriteLine("Roll Die value is : " + DieValue);
-                Player1 = obj.Check_Option(DieValue, Player1);
-                Console.WriteLine("Player Status And Value is : " + Player1);
+                int PrePostion = Player1;
+                Player1 = obj.Check_Option(DieValue, Player1, PrePostion);
 
+                if (Player1 > 100)
+                {
+                    Player1 = PrePostion;
+                }
+                Console.WriteLine("Player 1 Position is : " + Player1);
             } 
         }
 
